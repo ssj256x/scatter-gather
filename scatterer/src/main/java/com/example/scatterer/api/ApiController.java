b23078bb71b6530e3caa0199e73a5a7ddcc17abd
Sanjeet Jagdev <ssj256x@outlook.com>
2026-06-24 10:53:36 +0530

    Added optimizations

--- scatterer/src/main/java/com/example/scatterer/api/ApiController.java
+++ scatterer/src/main/java/com/example/scatterer/api/ApiController.java
@@ -9,26 +9,25 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+
 @Log4j2
 @RestController
+@RequestMapping(
+        value = "/api/v1/scatter",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE
+)
 @RequiredArgsConstructor
 public class ApiController {
 
     private final SimpleProducer simpleProducer;
     private final RandomIdService randomIdService;
 
-    @PostMapping(
-            value = "create",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @PostMapping("create")
     public ResponseEntity<BatchResponse> createScatter(@RequestBody BatchRequest request,
                                                        @RequestParam(value = "random-ids", required = false) boolean randomIds,
                                                        @RequestParam(required = false) int size) {
--- worker/src/main/java/com/example/worker/WorkerApplication.java
+++ worker/src/main/java/com/example/worker/WorkerApplication.java
@@ -2,8 +2,15 @@ package com.example.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class,
+		ValidationAutoConfiguration.class
+})
 public class WorkerApplication {
 
 	public static void main(String[] args) {
